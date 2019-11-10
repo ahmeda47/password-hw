32e8c44e806482186cca4bd69edf4d8e3aa65463
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2019-11-10 15:43:30 -0500

    added event listener to password generating button

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "U+005C", "]", "^", "_", "`", "{", "|", "}", "~", ];
-//var passLength = prompt("How long should the password be?");
-var passType =  prompt("Which types do you want?");
+// var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", ];
+// var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//var upperLower = [...upperCase, ...lowerCase];
+//var upperLowerSpecial = [...upperCase, ...lowerCase, ...specialChar];
+//var upperLowerSpecialNum = [...upperCase, ...lowerCase, ...specialChar, ...num];
+var click1 = document.querySelector("#type1");
+var click2 = document.querySelector("#type2");
+var click3 = document.querySelector("#type3");
+var click4 = document.querySelector("#type4");
+var generateClick = document.querySelector("#generate");
+var mixedPass = document.querySelector("#password")
 
-if (passType === "uppercase"){
-    alert(upperCase);
-}
-//alert(passType);
 
+generateClick.addEventListener("click", function(){
+    var checks = document.getElementsByClassName("check");
+    for (var i = 0; i < 4; i++){
+        if (checks[i].checked){
+            mixedPass.innerHTML += checks[i].value;
+        }
+    }
+});
 
-//if someone selects 1,2,3, or 4 types then add up respective number of arrays
 //randomizer should equal the total of the selected types' elements
-//need for loop
-
